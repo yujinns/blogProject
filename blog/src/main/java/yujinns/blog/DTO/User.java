fbe54ae8860a5c437eab7dfa2f269d91f2e8c20b
sequaa <asl0619@naver.com>
2023-08-28 14:11:12 +0900

    create: UserDTO

---
+++ blog/src/main/java/yujinns/blog/DTO/User.java
@@ -0,0 +1,16 @@
+package yujinns.blog.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class User {
+
+    private String id;
+    private String password;
+}
