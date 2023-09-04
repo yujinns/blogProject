8b94a92ee1c8ec99a461c07b15c86d0682ee5594
cherimlee <pinkcherim@gmail.com>
2023-09-04 12:24:04 +0900

    create: CommentMapper

---
+++ blog/src/main/java/yujinns/blog/mapper/CommentMapper.java
@@ -0,0 +1,22 @@
+package yujinns.blog.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+import yujinns.blog.DTO.Comment;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface CommentMapper {
+
+    List<Comment> getAllComments();
+
+    void insertComment(Comment comment);
+
+    Comment selectCommentById(String id);
+
+    void updateCommentById(Comment comment);
+
+    void deleteCommentById(String id);
+}
