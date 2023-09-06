151f2e5463f40f92eda433e938512dfa99d774b0
Gyulo94 <135434751+Gyulo94@users.noreply.github.com>
2023-09-06 09:53:09 +0900

    update : Controller, DTO, Service, Mapper 기본적인 CRUD

--- blog/src/main/java/yujinns/blog/DTO/Board.java
+++ blog/src/main/java/yujinns/blog/DTO/Board.java
@@ -1,4 +1,22 @@
 package yujinns.blog.DTO;
 
+import lombok.*;
+
+import java.sql.Timestamp;
+
+
+@Data
+@Getter
+@Setter
 public class Board {
+
+    private int boardIdx;
+    private String title;
+    private String content;
+    private String name;
+    private String pwd;
+    private Timestamp createdtime;
+    private int hitcnt;
+    private Timestamp updatedtime;
+
 }
