c60b97200a88c4325aa13ae6f22e481509883f72
elizabethgim <yujingim@naver.com>
2023-09-13 16:40:45 +0900

    fix: controller tag 추가

--- blog/src/main/java/yujinns/blog/controller/IndexController.java
+++ blog/src/main/java/yujinns/blog/controller/IndexController.java
@@ -1,7 +1,9 @@
 package yujinns.blog.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class IndexController {
     @GetMapping("/home")
     public String home() { return "/home"; }
