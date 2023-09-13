af9f2a8eebb385e1417b6326509c092ff00e9b06
elizabethgim <yujingim@naver.com>
2023-09-13 09:39:07 +0900

    fix: update 수정

--- blog/src/main/java/yujinns/blog/controller/BoardController.java
+++ blog/src/main/java/yujinns/blog/controller/BoardController.java
@@ -39,56 +39,63 @@ public class BoardController {
         return "elden_ring_detail";
     }
 
-    @GetMapping("/readform")
-    public String boardDetail(HttpServletRequest request,
-                              HttpServletResponse response, int boardIdx, Model model) {
+//    @GetMapping("/details")
+//    public String showBoardDetails(int boardIdx, Model model){
+//        Board board = boardService.selectByIdx(boardIdx);
+//        model.addAttribute("board", board);
+//        return "elden_ring_detail";
+//    }
 
-        // 쿠키에서 이미 조회한 게시글 ID를 확인
-        boolean isViewed = false;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if ("viewedBoardIds".equals(cookie.getName())) {
-                    String[] viewedIds = cookie.getValue().split(",");
-                    for (String viewedId : viewedIds) {
-                        if (viewedId.equals(String.valueOf(boardIdx))) {
-                            isViewed = true;
-                            break;
-                        }
-                    }
-                    break;
-                }
-            }
-        }
-
-        // 이미 조회한 게시글이 아니라면 조회수를 증가하고 쿠키에 게시글 ID를 추가
-        if (!isViewed) {
-            boardService.updatehit(boardIdx);
-
-            // 쿠키에 게시글 ID 추가
-            String viewedIds = "";
-            Cookie[] existingCookies = request.getCookies();
-            if (existingCookies != null) {
-                for (Cookie cookie : existingCookies) {
-                    if ("viewedBoardIds".equals(cookie.getName())) {
-                        viewedIds = cookie.getValue() + ",";
-                        break;
-                    }
-                }
-            }
-            viewedIds += boardIdx;
-
-            Cookie cookie = new Cookie("viewedBoardIds", viewedIds);
-            cookie.setMaxAge(3600 * 24); // 쿠키 유효기간 설정 (하루: 3600초 * 24시간)
-            response.addCookie(cookie);
-        }
-
-        Board dto = boardService.selectByIdx(boardIdx);
-        model.addAttribute("read", dto);
-        System.out.println(dto);
-
-        return "readform";
-    }
+//    @GetMapping("/readform")
+//    public String boardDetail(HttpServletRequest request,
+//                              HttpServletResponse response, int boardIdx, Model model) {
+//
+//        // 쿠키에서 이미 조회한 게시글 ID를 확인
+//        boolean isViewed = false;
+//        Cookie[] cookies = request.getCookies();
+//        if (cookies != null) {
+//            for (Cookie cookie : cookies) {
+//                if ("viewedBoardIds".equals(cookie.getName())) {
+//                    String[] viewedIds = cookie.getValue().split(",");
+//                    for (String viewedId : viewedIds) {
+//                        if (viewedId.equals(String.valueOf(boardIdx))) {
+//                            isViewed = true;
+//                            break;
+//                        }
+//                    }
+//                    break;
+//                }
+//            }
+//        }
+//
+//        // 이미 조회한 게시글이 아니라면 조회수를 증가하고 쿠키에 게시글 ID를 추가
+//        if (!isViewed) {
+//            boardService.updatehit(boardIdx);
+//
+//            // 쿠키에 게시글 ID 추가
+//            String viewedIds = "";
+//            Cookie[] existingCookies = request.getCookies();
+//            if (existingCookies != null) {
+//                for (Cookie cookie : existingCookies) {
+//                    if ("viewedBoardIds".equals(cookie.getName())) {
+//                        viewedIds = cookie.getValue() + ",";
+//                        break;
+//                    }
+//                }
+//            }
+//            viewedIds += boardIdx;
+//
+//            Cookie cookie = new Cookie("viewedBoardIds", viewedIds);
+//            cookie.setMaxAge(3600 * 24); // 쿠키 유효기간 설정 (하루: 3600초 * 24시간)
+//            response.addCookie(cookie);
+//        }
+//
+//        Board dto = boardService.selectByIdx(boardIdx);
+//        model.addAttribute("read", dto);
+//        System.out.println(dto);
+//
+//        return "readform";
+//    }
 
     @GetMapping("/writeform")
     public String writeform(){
@@ -102,20 +109,19 @@ public class BoardController {
         return "redirect:/board";
     }
 
-    @GetMapping("/updateform")
-    public ModelAndView updateform(int boardIdx) throws Exception{
-        Board dto = boardService.selectByIdx(boardIdx);
-        ModelAndView mv = new ModelAndView("/updateform");
+    @GetMapping("/updateform/{id}")
+    public String updateform(@PathVariable int id, Model model) throws Exception{
+        Board dto = boardService.selectByIdx(id);
         System.out.println(dto);
-        mv.addObject("update", dto);
-        return mv;
+        model.addAttribute("update", dto);
+        return "elden_ring_updateForm";
     }
 
     @PostMapping("/updateAction")
     public String updateAction(Board dto) throws Exception {
         System.out.println("yujinns update::"+ dto);
         boardService.updateAction(dto);
-        return "redirect:/board/list";
+        return "redirect:/board";
     }
 
     @GetMapping("/deleteBoard")
